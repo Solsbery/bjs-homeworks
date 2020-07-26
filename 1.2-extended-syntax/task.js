ded7930914428a659c839ffab4432e54d7116f62
Nadezhda Shulga <por.que@yandex.ru>
2020-07-26 21:02:16 +0300

    Homework 1.2 second commit

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -6,56 +6,40 @@ function getResult(a,b,c){
     let D = b ** 2 - 4 * a * c;
 
     if (D < 0) {
-    	x = [];
+    	return [];
     } else if (D === 0) {
-    	x = [-b / 2 * a];
+    	return [-b / 2 * a];
     } else {
-    	x = [(-b + Math.sqrt(D)) / (2 * a), (-b - Math.sqrt(D)) / (2 * a)];
+    	return [(-b + Math.sqrt(D)) / (2 * a), (-b - Math.sqrt(D)) / (2 * a)];
     }
-	
-    return x;
 }
 
 function getAverageMark(marks){
     // код для задачи №2 писать здесь
-    let averageMark;
     let marksSum = 0;
-    
-    function calcAverageMark() {
-    	for (let i = 0; i < marks.length; i++) {
-    		marksSum += marks[i]
-    	};
-    	averageMark = marksSum / marks.length;
-    }
-    
 
     if (marks.length == 0) {
-    	averageMark = 0;
-    } else  if (marks.length <= 5) {
-    	calcAverageMark();
-    } else {
+    	return 0;
+    } else if (marks.length > 5) {
     	console.log('Количество оценок больше 5.');
-    	marks = marks.slice(0,5);
-    	calcAverageMark();
-    }
+    	marks.splice(5);
+    } 
+
+    for (let i = 0; i < marks.length; i++) {
+    		marksSum += marks[i];
+    	};
+    let averageMark = marksSum / marks.length;
 
     return averageMark;
 }
 
 function askDrink(name,dateOfBirthday){
     // код для задачи №3 писать здесь
-
-    let yearOfBirth = dateOfBirthday.getFullYear();
-    const year = new Date().getFullYear();
-    let age = year - yearOfBirth;
-    let result;
+    let age = new Date().getFullYear() - dateOfBirthday.getFullYear();
 
     if (age >= 18) {
-    	result = ['Не желаете ли олд-фэшн', name];
+    	return `Не желаете ли олд-фэшн, ${name}?`;
     } else {
-    	result = ['Сожалею', name, 'но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!']; 
+    	return `Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`; 
     }
-    
-
-    return result;
 }
