fed2eaccaf979fe84a22d4176c25c66b9a161fab
Nadezhda Shulga <por.que@yandex.ru>
2020-07-31 00:17:21 +0300

    Homework 1.3 first commit

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,40 @@
+'use strict';
+
 function calculateTotalMortgage(percent, contribution, amount, date) {
     // код для задачи №1 писать здесь
-    // return totalAmount;
+
+   // if (typeof percent === 'string') {
+   // 	parseFloat(percent);
+   // } else if (percent == NaN) {
+   // 	return `Параметр <процентная ставка> содержит неправильное значение ${percent}`;
+   // }
+
+   // if (typeof contribution === 'string') {
+   // 	parseFloat(contribution);
+   // } else if (typeof percent != 'number') {
+   // 	return `Параметр <сумма первоначального взноса> содержит неправильное значение ${contribution}`;
+   // }
+
+   // if (typeof amount === 'string') {
+   // 	parseFloat(amount);
+   // } else if (typeof percent != 'number') {
+   // 	return `Параметр <сумма кредита> содержит неправильное значение ${amount}`;
+   // }
+
+   let creditAmount = amount - contribution;
+   let term = (date.getFullYear() - new Date().getFullYear()) * 12 + (date.getMonth() - new Date().getMonth());
+   let monthPercent = percent / 100 / 12;
+   let monthPayment = creditAmount * (monthPercent + monthPercent / (((1 + monthPercent) ** term) - 1));
+   let totalAmount = monthPayment * term;
+   console.log(totalAmount.toFixed(2));
+
+   return parseFloat(totalAmount.toFixed(2));
 }
 
 function getGreeting(name) {
     // код для задачи №2 писать здесь
-    // return greeting;
+
+    if (name == false) name = 'Аноним'
+
+    return `Привет, мир! Меня зовут ${name}.`;
 }
