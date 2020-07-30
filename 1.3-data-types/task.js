b7e1f0d205610d5c1744b1d906c730722c8a1681
Nadezhda Shulga <por.que@yandex.ru>
2020-07-31 00:17:21 +0300

    Second commit

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,37 @@
+'use strict';
+
 function calculateTotalMortgage(percent, contribution, amount, date) {
     // код для задачи №1 писать здесь
-    // return totalAmount;
+    
+   if (isNaN(percent)) {
+   	return `Параметр <процентная ставка> содержит неправильное значение ${percent}`;
+   }
+   	const parsedPercent = parseFloat(percent);
+
+   if (isNaN(contribution)) {
+   	return `Параметр <сумма первоначального взноса> содержит неправильное значение ${contribution}`;
+   }
+   	const parsedContribution = parseFloat(contribution);
+
+   if (isNaN(amount)) {
+   	return `Параметр <сумма кредита> содержит неправильное значение ${amount}`;
+   }
+   	const parsedAmount = parseFloat(amount);
+
+   let creditAmount = amount - contribution;
+   let term = (date.getFullYear() - new Date().getFullYear()) * 12 + (date.getMonth() - new Date().getMonth());
+   let monthPercent = percent / 100 / 12;
+   let monthPayment = creditAmount * (monthPercent + monthPercent / (((1 + monthPercent) ** term) - 1));
+   let totalAmount = monthPayment * term;
+   console.log(totalAmount.toFixed(2));
+
+   return parseFloat(totalAmount.toFixed(2));
 }
 
 function getGreeting(name) {
     // код для задачи №2 писать здесь
-    // return greeting;
+
+    // if (name == false) name = 'Аноним'
+
+    return `Привет, мир! Меня зовут ${name || 'Аноним'}.`;
 }
