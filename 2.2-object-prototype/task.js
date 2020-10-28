bfa482ce35abfd9145a7f0eddb406a59fb6d3fe2
Nadezhda Shulga <por.que@yandex.ru>
2020-10-28 23:21:36 +0300

    First commit

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,11 +1,39 @@
-//String.prototype.isPalindrome - для задачи №1
+//Задача 1
+String.prototype.isPalindrome = function() {
 
+	let string = this.toLowerCase().split(' ').join('');
+
+	let arrString = [];
+	for (let i = 0; i < string.length; i++) {
+		arrString.push(string.charAt(i));
+	}
+	let reverseString = [];
+	for (let i = string.length - 1; i >= 0; i--) {
+		reverseString.push(string.charAt(i));
+	}
+
+	if (arrString.join('') === reverseString.join('')) return true;
+	return false
+	
+}
+console.log(this.isPalindrome);
+
+//Задача 2
 function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+	let marksSum = 0;
+
+    for (let i = 0; i < marks.length; i++) {
+    	marksSum += marks[i];
+    };
+
+    let roundedAverage = Math.round(marksSum / marks.length);
+    return roundedAverage
 }
 
+//Задача 3
 function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
+	const now = Date.now();
+	birthday = birthday.Date();
+	console.log(birthday);
     // return verdict
 }
